b02bfa6ef73f5967c347142ba9bf6374fe74ee34
Humphrey Obuobi <obuobhum@gmail.com>
2016-10-09 20:54:06 -0400

    initial commit

---
+++ index.html
@@ -0,0 +1,20 @@
+<DOCTYPE! html>
+	<html>
+		
+	<head>
+		<title>Profile Picture Framer</title>
+		<!-- CSS -->
+		
+		<!-- JS -->
+		<script src="js/jquery.min.js"></script>
+	</head>
+	<body>
+		
+		<label>Image File:</label><br/>
+		<input type="file" id="imageLoader" name="imageLoader"/>
+		<button type="button" id="download">DOWNLOAD</button>
+		<canvas id="imageCanvas"></canvas>
+	</body>
+	<script src="js/main.js"></script>
+	</html>
+
---
+++ js/main.js
@@ -0,0 +1,44 @@
+var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+var downloader = document.getElementById('download');
+    downloader.addEventListener('click', downloadProfile, false);
+var canvas = document.getElementById('imageCanvas');
+var ctx = canvas.getContext('2d');
+
+
+function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        var frameImg = new Image();
+        var padding = 20;
+        frameImg.src = 'assets/IL_frameImg.png';
+        frameImg.crossOrigin="anonymous"
+        img.crossOrigin="anonymous"
+        ctx.drawImage(frameImg, 0, 0);
+        img.onload = function(){
+            canvas.width = 1000;
+            canvas.height = 1000;
+            ctx.drawImage(img,0,0,1000,1000);
+            ctx.drawImage(frameImg,1000-frameImg.width-padding,1000-frameImg.height-padding);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+
+    /*
+      imageObj.onload = function() {
+        ctx.drawImage(imageObj, 0, 0);
+      };
+      imageObj.src = 'assets/IL_frameImg.png';
+      */
+}
+
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+function downloadProfile() {
+    downloadCanvas(this, 'imageCanvas', 'profileFramed.png');
+}
